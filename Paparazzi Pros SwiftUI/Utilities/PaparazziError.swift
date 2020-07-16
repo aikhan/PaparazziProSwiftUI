229d1136c24b035c2193d8435789c5f426993e1c
Asad Khan <asadkhan.khan@gmail.com>
2020-07-16 23:05:44 +0500

    MVVM architecture

---
+++ Paparazzi Pros SwiftUI/Utilities/PaparazziError.swift
@@ -0,0 +1,9 @@
+//
+//  PaparazziError.swift
+//  Paparazzi Pros SwiftUI
+//
+//  Created by Asad Khan on 16/07/2020.
+//  Copyright © 2020 Paparazzi Pros. All rights reserved.
+//
+
+import Foundation
